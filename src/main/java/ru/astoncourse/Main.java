16ae8591f73fce39198752b02fd328627aa0f22b
Егор Мазков <mazkovegor@mail.ru>
2025-08-07 12:18:59 +0300

    lesson_2

---
+++ src/main/java/ru/astoncourse/Main.java
@@ -0,0 +1,49 @@
+package ru.astoncourse;
+
+import static ru.astoncourse.tasks.Task_1.printThreeWords;
+import static ru.astoncourse.tasks.Task_2.checkSumSing;
+import static ru.astoncourse.tasks.Task_3.printColor;
+import static ru.astoncourse.tasks.Task_4.compareNumbers;
+import static ru.astoncourse.tasks.Task_5.isSumInRange;
+import static ru.astoncourse.tasks.Task_6.printNumberSign;
+import static ru.astoncourse.tasks.Task_7.printBooleanNumberSign;
+import static ru.astoncourse.tasks.Task_8.printStringMultipleTimes;
+import static ru.astoncourse.tasks.Task_9.isLeapYear;
+import static ru.astoncourse.tasks.Task_10.invertBinaryArray;
+import static ru.astoncourse.tasks.Task_11.fillArrayWithSequence;
+import static ru.astoncourse.tasks.Task_12.updateValuesLessThanSix;
+import static ru.astoncourse.tasks.Task_13.fillMainDiagonalWithOnes;
+import static ru.astoncourse.tasks.Task_14.createArrayWithValue;
+
+public class Main {
+    public static void main(String[] args) {
+        printThreeWords(); // Task_1
+        System.out.println();
+        checkSumSing(); // Task_2
+        System.out.println();
+        printColor(); // Task_3
+        System.out.println();
+        compareNumbers(); // Task_4
+        System.out.println();
+        isSumInRange(); // Task_5
+        System.out.println();
+        printNumberSign(); // Task_6
+        System.out.println();
+        printBooleanNumberSign(); // Task_7
+        System.out.println();
+        printStringMultipleTimes(); // Task_8
+        System.out.println();
+        isLeapYear(); // Task_9
+        System.out.println();
+        invertBinaryArray(); // Task_10
+        System.out.println();
+        fillArrayWithSequence(); // Task_11
+        System.out.println();
+        updateValuesLessThanSix(); // Task_12
+        System.out.println();
+        fillMainDiagonalWithOnes(); // Task_13
+        System.out.println();
+        createArrayWithValue(); // Task_14
+        System.out.println();
+    }
+}
