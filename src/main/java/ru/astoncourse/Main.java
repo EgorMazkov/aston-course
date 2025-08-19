050a068a113d176a6029264f79aa7bef25424336
Егор Мазков <mazkovegor@mail.ru>
2025-08-19 10:51:50 +0300

    lesson_5

--- src/main/java/ru/astoncourse/Main.java
+++ src/main/java/ru/astoncourse/Main.java
@@ -1,17 +1,55 @@
 package ru.astoncourse;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import ru.astoncourse.exceptions.MyArrayDataException;
+import ru.astoncourse.exceptions.MyArraySizeException;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        String[][] array = new String[][]{
+                {"1", "2", "3", "4"},
+                {"5", "6", "7", "8"},
+                {"9", "10", "11", "12"},
+                {"13", "14", "F", "16"}
+        };
+
+        validatorArraySize(array);
+        generateAndCatchArrayIndexOutOfBounds();
+    }
+
+    private static void generateAndCatchArrayIndexOutOfBounds() {
+        int[] array = new int[4];
+        try {
+            array[5] = 1;
+        } catch (ArrayIndexOutOfBoundsException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void validatorArraySize(String[][] array) {
+        int sumArray = 0;
+        for (int i = 0, j = 0; i < array.length; i++) {
+                if (array[i].length != 4) {
+                    throw new MyArraySizeException("Ошибка: Неверный размер массива");
+                }
+                for (j = 0; j < array[i].length; j++) {
+                    try {
+                        sumArray += getParseInt(i, j, array);
+                    } catch (MyArrayDataException e) {
+                        e.printStackTrace();
+                    }
+                }
+                if (i == 4 && j == 4) {
+                    System.out.println("Сумма элементов: " + sumArray);
+                }
+        }
+    }
+
+    private static int getParseInt(int i, int j, String[][] array) throws MyArrayDataException {
+        try {
+            return Integer.parseInt(array[i][j]);
+        } catch (NumberFormatException e) {
+            throw new MyArrayDataException("Ошибка: Обнаружен спец. символ или буква [ " + i + " ][ " + j + " ]");
         }
     }
 }
---
+++ src/main/java/ru/astoncourse/exceptions/MyArrayDataException.java
@@ -0,0 +1,7 @@
+package ru.astoncourse.exceptions;
+
+public class MyArrayDataException extends NumberFormatException {
+    public MyArrayDataException(String message) {
+        super(message);
+    }
+}
